9f964fd530400aa76601363b45acc3cea31fa9d3
jerrold white <liljerrold03@gmail.com>
2017-07-18 15:02:52 -0400

    added stuff

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -11,8 +11,19 @@ module.exports = {
         // *********************************************************************
         // CODE HERE!
         // *********************************************************************
+var lc = message.toLowerCase();
+        
+if (lc.includes('homework')) {
+    Slack.postMessageToChannel(channelName, 'Why dont you use your notes you got at school instead of asking me.');
+}
+else {
+    Slack.postMessageToChannel(channelName, 'WHAT DO YOU WANT!!!!! I AM TRYING TO HAVE A GOOD DAY TILL YOU SHOWED UP THANKS FOR RUINING MY DAY!!!!.');
+}
+if (lc.includes('dumb')){
+    Slack.postMessageToChannel(channelName, 'Your the dumb one now leave me alone');
+}
 
-        Slack.postMessageToChannel(channelName, 'Heard your cry for help.');
+
 
         // *********************************************************************
         // STOP CODING!
