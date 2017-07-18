814f5e8b8724150c6d98305da3fd5ddcf44ee694
Marcquis Mikes <mikehenny126@outlook.com>
2017-07-18 14:59:12 -0400

    Bored Comittee

--- CodeCamp-Bored.js
+++ CodeCamp-Bored.js
@@ -3,7 +3,7 @@
 module.exports = {
     settings: {
         enabled: true,
-        timeoutInSeconds: 50,
+        timeoutInSeconds: 15,
     },
 
     /**
@@ -14,8 +14,19 @@ module.exports = {
         // *********************************************************************
         // CODE HERE!
         // *********************************************************************
+        var helper = require('./helpers');
 
-        Slack.postMessageToChannel(channel, 'Can you respond? Or are you too stupid to do that.');
+        switch (helper.numberBetweenXandY(1, 4)) {
+            case 1: Slack.postMessageToChannel(channel, 'Hurry!!! I AM WAITINGGG! HELLOOO!');
+                break;
+            case 2: Slack.postMessageToChannel(channel, 'Im thinking of a "number" between HURRY and UP!, Right NOW!');
+                break;
+            case 3: Slack.postMessageToChannel(channel, 'Are you still there? Cause if not BYE!');
+                break;
+            case 4: Slack.postMessageToChannel(channel, 'TIME is of the ESSENCE, hint hint...!');
+                break;
+        }
+        Slack.postMessageToChannel(channel, 'Hurry!!! I AM WAITINGGG! HELLOOO!');
 
         // *********************************************************************
         // STOP CODING!
