536179356281273e3163e3b640f0073ace3093bb
Laasya Challa <mayflowers1620@gmail.com>
2017-07-18 14:41:28 -0400

    Weather Update

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -11,11 +11,13 @@ module.exports = {
        var lc = question.toLowerCase();
        var response = 'Well.....';
 
-        if (lc.includes('when') && lc.includes('your') && lc.includes('birthday')){
+        if (lc.includes('when') && lc.includes('your') && lc.includes('birthday')) {
         response = "Why do you care? You're not invited to my birthday party anyway.";
         }
         
-            
+        if (lc.includes('weather') && lc.includes('what')) {
+            response = "I don't know. Why don't you try going outside and checking?";
+        }
         // *********************************************************************
 
         Slack.postMessageToChannel(channelName, response);
