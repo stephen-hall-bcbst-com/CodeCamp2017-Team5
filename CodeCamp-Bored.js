a04f459bdbf1b724a77c6aeff63ce65449e39445
Marcquis Mikes <mikehenny126@outlook.com>
2017-07-19 12:50:43 -0400

    Extra Quotes

--- CodeCamp-Bored.js
+++ CodeCamp-Bored.js
@@ -16,7 +16,7 @@ module.exports = {
         // *********************************************************************
         var helper = require('./helpers');
         // Response(s) when bot gets bored at a given time, (15) seconds
-        switch (helper.numberBetweenXandY(1, 4)) {
+        switch (helper.numberBetweenXandY(1, 6)) {
             case 1: Slack.postMessageToChannel(channel, 'What do you need? Oh, and remember to drink 8, 8 ounce cups of water each day');
                 break;
             case 2: Slack.postMessageToChannel(channel, 'Im thinking of a "number" between HURRY and UP!, Right NOW!');
@@ -27,6 +27,7 @@ module.exports = {
                 break;
             case 5: Slack.postMessageToChannel(channel, 'Where did you go Honey? Still there?');
                 break; 
+            case 6: Slack.postMessageToChannel(channel, 'Are you shy sugar? Say something..');
         }
         
 
