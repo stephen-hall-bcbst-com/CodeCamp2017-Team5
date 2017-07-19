3e6f98c13c593cd1303618700d5c3f935ad3f8de
Laasya Challa <mayflowers1620@gmail.com>
2017-07-19 12:35:50 -0400

    Updates to Marge's Favorite things

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -21,10 +21,16 @@ if (lc.includes('favorite')) {
                  response = "mmMMm. I love me some prune puddin'. I'll make ya some when me and mah cats to visit ya!";
         }
             if (text.includes('color')) {
-                 response = 'Magenta, of course! I bought the cutest magenta hat last saturday!';
+                 response = 'Magenta, of course! I bought the cutest magenta sun hat last saturday! Large and covered in frills with a giant feather on top!';
         }      
             if (text.includes('animal')) {
                  response = "Favorite animal? Well, mah cats of course! I love mah furry babies more then I love plum puddin'! ";
+        }   
+            if (text.includes('drink')) {
+                 response = "I love me a big ol' jug of pink lemonade! Perfect for days lounging under the sun with mah new magenta hat.";
+        }
+            if (text.includes('show')) {
+                 response = "Dance Moms! Those ladies keep mah young, with their cracka lackin' and their pop hop!";
         }
     }
 }
