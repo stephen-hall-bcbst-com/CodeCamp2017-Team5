90426421d809e3dde45a4047fbf9f49293db9c02
Laasya Challa <mayflowers1620@gmail.com>
2017-07-19 14:43:23 -0400

    small fix

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -62,7 +62,7 @@ if (lc.includes('favorite')) {
  }
 
 // Suffles through 3 responses to send when asked "What you doing?"
-if (lc.includes('what') && lc.includes('you') && lc.includes('doing')); {
+if (lc.includes('what') && lc.includes('you') && lc.includes('doing')) {
     Slack.postMessageToChannel(channelName, helper.phraseAtRandom(WydPhrases));
  }
 
