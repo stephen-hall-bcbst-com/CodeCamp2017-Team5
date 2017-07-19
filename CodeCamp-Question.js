05cdf5d33a07f78f4b2b7b35510211f87548a8c6
Laasya Challa <mayflowers1620@gmail.com>
2017-07-19 11:34:46 -0400

    Quick Update with checktext function

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -12,28 +12,11 @@ module.exports = {
        var lc = question.toLowerCase();
        var response = 'well..';
 
-// Response when questioned about birthday.
-        if (lc.includes('when') && lc.includes('your') && lc.includes('birthday')) {
-        response = "Why do you care? You're not invited to my birthday party anyway.";
-    }
-    
-// Response when questioned about weather.
-        if (lc.includes('weather') && lc.includes('what')) {
-            response = "I don't know. Why don't you try going outside and try checking?";
-    }
-
-// Response when questioned about mean behavior.
-        if (lc.includes('why') && lc.includes('you') && lc.includes('mean')) {
-            response = "Well, I have to be around you all day.";
-    }
-
 // Questions will be runned through 
     if (lc.includes('favorite')) {
-        checkforworld1(lc);
-    }
-
-        
-        function checkforworld1(text) {
+          checktext(lc);
+           
+        function checktext(text) {
             switch (text) {
                case 'food': 
                     response = 'Strawberry Pie';
