3aab4b3e5b1fbef6f740e155a1dbf784302b700a
Jerald Arden Freeman <FrozenSeed474@gmail.com>
2017-07-19 10:04:26 -0400

    message

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -14,20 +14,19 @@ module.exports = {
 
         var lc = message.toLowerCase();
 
-      }  if (lc.includes('know')) {
+             if (lc.includes('know')) {
             Slack.postMessageToChannel(channelName, 'Who cares about that anyway... just go away...');
       } else if (lc.includes('joke')) {
             Slack.postMessageToChannel(channelName, 'Joke? the only joke here is you!');
       } else if (lc.includes('robot')) {
             Slack.postmessageToChannel(channelName, 'Heh... im not a ro..bot');
-      } else if (lc.includes('girl')) {
-            Slack.postmessageToChannel(channelName, 'Yeah Im married, see...  ');
       } else if (lc.includes('depression')) {
             Slack.postmessageToChannel(channelName, 'Yeah I got that speaking to you!');
       } else if (lc.includes('nice')) {  
             Slack.postmessageToChannel(channelName, 'Hey you are kinda nice thanks, but that changes NOTHING!');
       } else if (lc.includes('hey')) { 
             Slack.postmessageToChannel(channelName, 'Hey can I leave!');
+      
       
 
         
