b4afabe248432d8b0700fa545f9ece436930a3fd
Laasya Challa <mayflowers1620@gmail.com>
2017-07-19 09:54:20 -0400

    gfgf

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -8,22 +8,42 @@ module.exports = {
      */
     question_received: function(question, channelName, userName, Slack) {
         // *********************************************************************
+      
        var lc = question.toLowerCase();
        var response = 'well..';
 
+// Response when questioned about birthday.
         if (lc.includes('when') && lc.includes('your') && lc.includes('birthday')) {
         response = "Why do you care? You're not invited to my birthday party anyway.";
-        }
-        
+    }
+    
+// Response when questioned about weather.
         if (lc.includes('weather') && lc.includes('what')) {
             response = "I don't know. Why don't you try going outside and try checking?";
-        }
+    }
 
+// Response when questioned about favorite color.
         if (lc.includes('favorite') && lc.includes('color')) {
             response = "Any color that you don't like.";
-        }
+    }
 
-        if (lc.includes)
+// Response when questioned about mean behavior.
+        if (lc.includes('why') && lc.includes('you') && lc.includes('mean')) {
+            response = "Well, I have to be around you all day.";
+    }
+
+// Response when questioned about favorite things
+        if (lc.includes('favorite')) {
+        
+            switch(text) {
+               case 'food' : 
+                    Slack.postMessageToChannel(channelName, 'Strawberry Pie');
+                    break;
+               case 'color' :
+                    Slack.postMessageToChannel(channelName, 'Green');
+                    break;
+        }
+        
 
 
         // *********************************************************************
