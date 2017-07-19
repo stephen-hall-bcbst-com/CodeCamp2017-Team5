f8a8ee43dcce5c26d2f43e17b8407ba7ce087a4d
Laasya Challa <mayflowers1620@gmail.com>
2017-07-19 10:03:18 -0400

    vv

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -33,16 +33,20 @@ module.exports = {
     }
 
 // Response when questioned about favorite things
-    if (lc.includes('favorite'));
+    if (lc.includes('favorite')) {
+        checkforworld1(lc);
+    }
+
         
         function checkforworld1(text) {
-            switch(text) {
-               case 'food' : 
+            switch (text) {
+               case 'food': 
                     Slack.postMessageToChannel(channelName, 'Strawberry Pie');
                     break;
-               case 'color' :
+               case 'color':
                     Slack.postMessageToChannel(channelName, 'Green');
                     break;
+            }
         }
         
         
