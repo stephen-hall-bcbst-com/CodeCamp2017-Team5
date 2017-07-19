6d99217da2158a4d1e4bb631b40dbc6bfc2aaade
Jerald Arden Freeman <FrozenSeed474@gmail.com>
2017-07-19 12:46:30 -0400

    randomizer program

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -13,13 +13,24 @@ module.exports = {
         // *********************************************************************
 
         var lc = message.toLowerCase();
-
+ 
         if (lc.includes('hi') || lc.includes('hey')) {
             Slack.postMessageToChannel(channelName, 'Hi there buttercup');
         }
+         else if (lc.includes('bye') || lc.includes('goodbye')) {
+            Slack.postMessageToChannel(channelName, 'Take care see you later honey');
+        }
+        else if (lc.includes('hungry') || lc.includes('starving')) {
+            Slack.postMessageToChannel(channelName, 'microwave the frozen nuggets hon.');
+        }
+        else if (lc.includes('next') || lc.includes('action')) {
 
-        //Slack.postMessageToChannel(channelName, "Message received.");
-
+      
+      
+      
+      
+      
+       //Slack.postMessageToChannel(channelName, "Message received.");
         // *********************************************************************
         // STOP CODING!
         // *********************************************************************
