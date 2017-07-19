3e849a5d19061be061a6147dfda33b2de822adc6
Jerald Arden Freeman <FrozenSeed474@gmail.com>
2017-07-19 14:47:02 -0400

    help

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -14,12 +14,11 @@ module.exports = {
 var lc = message.toLowerCase();
         
 if (lc.includes('homework')) {
-    Slack.postMessageToChannel(channelName, 'Why dont you use your notes you got at school instead of asking me.');
+    Slack.postMessageToChannel(channelName, 
 } else {
-    Slack.postMessageToChannel(channelName, 'WHAT DO YOU WANT!!!!! I AM TRYING TO HAVE A GOOD DAY TILL YOU SHOWED UP THANKS FOR RUINING MY DAY!!!!.');
-}
+    Slack.postMessageToChannel(channelName,
 if (lc.includes('dumb')) {
-    Slack.postMessageToChannel(channelName, 'Your the dumb one now leave me alone dumbo');
+    Slack.postMessageToChannel(channelName,
     }
 
 
