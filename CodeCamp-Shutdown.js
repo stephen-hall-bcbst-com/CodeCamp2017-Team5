15cc971032b98a856907f21678727417fffa4b67
Jerald Arden Freeman <FrozenSeed474@gmail.com>
2017-07-20 09:40:26 -0400

    changing comment

--- CodeCamp-Shutdown.js
+++ CodeCamp-Shutdown.js
@@ -10,7 +10,7 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
-        Slack.postMessageToChannel(channelName, "Shutdown received.");
+        Slack.postMessageToChannel(channelName, "");
 
         // *********************************************************************
         // STOP CODING!
