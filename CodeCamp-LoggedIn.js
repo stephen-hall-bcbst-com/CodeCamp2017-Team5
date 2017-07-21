06deec51caf74bb36fa3a6d501a5742fa8a72902
Laasya Challa <mayflowers1620@gmail.com>
2017-07-21 10:58:55 -0400

    Small fix

--- CodeCamp-LoggedIn.js
+++ CodeCamp-LoggedIn.js
@@ -14,7 +14,7 @@ module.exports = {
            // Takes parent and child name data and places it in Memory
            memory.parent = helper.readDataFromFile('parent name');
            memory.child = helper.readDataFromFile('child name');
-           var ImBackPhrases = ["Hi buttercup! Back from bingo! What is your parent and child name? Please answer seperately"];
+           var ImBackPhrases = ["Hi buttercup! Back from bingo! What is your parent and child name? Please answer seperately. (Enter 'My parent's name is -'. Then enter 'My name is -')"];
            {Slack.postMessageToChannel(channel, helper.phraseAtRandom(ImBackPhrases));
         }
 
